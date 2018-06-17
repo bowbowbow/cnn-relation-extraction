0a1fa604ab7a305dcb1e981ca3dbb7c7079821e0
bowbowbow <clsrn1581@gmail.com>
2018-06-18 02:36:13 +0900

    update crawler to wiki crawler

--- own_data/crawler.js
+++ own_data/crawler.js
@@ -1,15 +1,23 @@
-/**
- * Created by bowbowbow on 2018. 6. 18..
- */
 const superagent = require('superagent');
 const cheerio = require('cheerio');
 const _ = require('lodash');
-const urlHandler = require('url');
 const iconv = require('iconv-lite');
 const rp = require('request-promise');
+const fs = require('fs-promise');
+
+const EMPTY = '.\r';
 
 class Crawler {
   constructor() {
+  }
+  
+  async getWikiParagraphs(keyword) {
+    const $ = await this.getDollar(`https://ko.wikipedia.org/wiki/${keyword}`)
+    const paragraphs = [];
+    $('#mw-content-text p').each(function (i, el) {
+      paragraphs.push($(this).text());
+    });
+    return paragraphs;
   }
   
   /**
@@ -48,6 +56,56 @@ class Crawler {
     });
   }
 }
+
+function findSentence(paragraphs, sbj, obj) {
+  for (let j = 0; j < paragraphs.length; j++) {
+    const paragraph = paragraphs[j].split('\n').join('');
+    const sentences = paragraph.split('.');
+    
+    for (let k = 0; k < sentences.length; k++) {
+      if (sentences[k].indexOf(sbj) >= 0 && sentences[k].indexOf(obj) >= 0) {
+        return sentences[k] + '.';
+      }
+    }
+  }
+  return null;
+}
+
+async function run() {
+  const data = await fs.readFile('triples.nt', 'utf8');
+  const lines = data.split('\n');
+  
+  for (let i = 0; i < lines.length; i++) {
+    console.log(`progress ${i + 1}/${lines.length}(${((i + 1) / lines.length * 100).toFixed(2)}%)`);
+    const cols = lines[i].split('\t');
+    
+    const sbj = cols[0];
+    const obj = cols[2];
+    const relation = cols[1];
+    let sentence = cols[3];
+    
+    if (sentence !== EMPTY) continue;
+    const paragraphsSbj = await new Crawler().getWikiParagraphs(sbj);
+    const paragraphsObj = await new Crawler().getWikiParagraphs(obj);
+    
+    let tmp = findSentence(paragraphsSbj, sbj, obj);
+    if (tmp) {
+      sentence = tmp;
+    } else {
+      tmp = findSentence(paragraphsObj, sbj, obj);
+      if (tmp) sentence = tmp;
+    }
+    
+    if (sentence !== EMPTY) {
+      console.log('found :', sentence);
+    } else {
+      console.log('not found', cols);
+    }
+  }
+}
+
+run().then();
+
 
 // Module Exports...
 module.exports = Crawler;
